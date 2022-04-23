244b131159104367dd6de5ba05a73a0e8471b8da
Azathoth <vladislavutkin92@gmail.com>
2022-04-23 19:01:09 +0300

    Full complete Cabinet vuew.

--- src/page/ts/abstract/Person.ts
+++ src/page/ts/abstract/Person.ts
@@ -1,11 +1,5 @@
 export default abstract class Person {
-    first_name: String;
-    last_name: String;
-
-    constructor(first_name: String, last_name: String) {
-        this.first_name = first_name;
-        this.last_name = last_name;
-    }
+    constructor(protected first_name: String, protected last_name: String) {}
 
     get firstName(): String {
         return this.first_name;
--- src/page/ts/app.ts
+++ src/page/ts/app.ts
@@ -1,10 +1,13 @@
-
-document.addEventListener('load', () => {
-    console.log(1)
+console.log("Script loaded...");
+import CabinetFabric from "./cabinet/CabinetFabric";
+import CabinetInterface from "./interface/CabinetInterface";
+(() => {
     const href: string = document.location.href;
     const hrefSplit: string[] = href.split('/');
     if(hrefSplit[hrefSplit.length - 1] === "edit#end_form") {
         console.log("Мы в старом кабинете")
+        const oldCabinet: CabinetInterface = CabinetFabric.makeCabinet();
+        oldCabinet.init();
     } else {
         window.addEventListener("click", event => {
             const target: HTMLElement = <HTMLElement> event.target;
@@ -13,10 +16,12 @@ document.addEventListener('load', () => {
             if(target.classList.contains("_-packages-ui-kit-components-v2-Button--button--27Ont")
                 && hrefSplit[hrefSplit.length - 2] == "task") {
                 console.log("мы в новом кабинете");
+                const newCabinet: CabinetInterface = CabinetFabric.makeCabinet();
+                newCabinet.init();
             }
         })
     }
-})
+})();
 
 
 
